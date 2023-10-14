7fd08fb6ac1bd2d47557dfd40dfa6c2e11be0973
ema <mccoy39082@163.com>
2023-10-14 15:57:35 +0800

    feat: urn:fischless.design

---
+++ src/Common/Fischless.Design/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: XmlnsPrefix("urn:fischless.design", "fui")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design.Animations")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design.Controls")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design.Converters")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design.Markups")]
+[assembly: XmlnsDefinition("urn:fischless.design", "Fischless.Design.Themes")]
+
+[assembly: XmlnsPrefix("http://schemas.fischless.design/2023", "fui")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design.Animations")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design.Controls")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design.Converters")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design.Markups")]
+[assembly: XmlnsDefinition("http://schemas.fischless.design/2023", "Fischless.Design.Themes")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3673062b-46b3-4f65-be44-cca410a1614e")]
