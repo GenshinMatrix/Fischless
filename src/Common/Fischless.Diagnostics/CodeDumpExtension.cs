43fe6fa14f48ae6f4d9712bb8d2827f7ef72b420
ema <mccoy39082@163.com>
2023-10-26 11:27:20 +0800

    add: preparatory diagnostic lib

---
+++ src/Common/Fischless.Diagnostics/CodeDumpExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Fischless.Diagnostics;
+
+public static class CodeDumpExtension
+{
+    public static T DumpCodeDebug<T>(this T? obj, DumpStyle dumpStyle = DumpStyle.Console, bool isOn = true)
+    {
+#if DEBUG
+        if (isOn && Debugger.IsAttached)
+        {
+            string dump = ObjectDumper.Dump(obj, dumpStyle);
+            Debug.WriteLine(dump);
+        }
+#endif
+        return obj;
+    }
+
+    public static T DumpCodeDebug<T>(this T? obj, DumpOptions dumpOptions, bool isOn = true)
+    {
+#if DEBUG
+        if (isOn && Debugger.IsAttached)
+        {
+            string dump = ObjectDumper.Dump(obj, dumpOptions);
+            Debug.WriteLine(dump);
+        }
+#endif
+        return obj;
+    }
+}
---
+++ src/Common/Fischless.Diagnostics/TableDumpExtension.cs
@@ -0,0 +1,19 @@
+﻿using Dumpify;
+using System.Diagnostics;
+
+namespace Fischless.Diagnostics;
+
+public static class TableDumpExtension
+{
+    public static T DumpTableDebug<T>(this T? obj, bool isOn = true, string? label = null, int? maxDepth = null, IRenderer? renderer = null, bool? useDescriptors = null, ColorConfig? colors = null, MembersConfig? members = null, TypeNamingConfig? typeNames = null, TableConfig? tableConfig = null, OutputConfig? outputConfig = null)
+    {
+#if DEBUG
+        if (isOn && Debugger.IsAttached)
+        {
+            string dump = obj.DumpText(label, maxDepth, renderer, useDescriptors, colors, members, typeNames, tableConfig, outputConfig);
+            Debug.WriteLine(dump);
+        }
+#endif
+        return obj;
+    }
+}
