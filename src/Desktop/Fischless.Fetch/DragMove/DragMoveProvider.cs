dd39e2e2e3fbfc8ef1d229be3f42039c3d4e70da
ema <mccoy39082@163.com>
2023-11-22 01:48:12 +0800

    feat: enable drag move the borderless window

---
+++ src/Desktop/Fischless.Fetch/DragMove/DragMoveProvider.cs
@@ -0,0 +1,114 @@
+﻿using Fischless.Fetch.Launch;
+using Fischless.MouseCapture;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using Vanara.PInvoke;
+
+namespace Fischless.Fetch.DragMove;
+
+public static class DragMoveProvider
+{
+    internal static MouseHook MouseHook { get; private set; } = null!;
+
+    private static bool isEnabled = false;
+
+    public static bool IsEnabled
+    {
+        get => isEnabled;
+        set
+        {
+            if (isEnabled != value)
+            {
+                isEnabled = value;
+                if (value)
+                {
+                    latestLocation = null;
+                    isMouseDown = false;
+                    MouseHook.Start();
+                }
+                else
+                {
+                    MouseHook.Stop();
+                }
+            }
+        }
+    }
+
+    private static Point? latestLocation = null;
+    private static bool isMouseDown = false;
+
+    static DragMoveProvider()
+    {
+        MouseHook = new MouseHook();
+        MouseHook.MouseDown += OnMouseDown;
+        MouseHook.MouseUp += OnMouseUp;
+        MouseHook.MouseMove += OnMouseMove;
+    }
+
+    private static void OnMouseDown(object? sender, MouseEventArgs e)
+    {
+        isMouseDown = true;
+    }
+
+    private static void OnMouseUp(object? sender, MouseEventArgs e)
+    {
+        isMouseDown = false;
+    }
+
+    private static void OnMouseMove(object? sender, MouseEventArgs e)
+    {
+        if (latestLocation == e.Location)
+        {
+            return;
+        }
+
+        if (latestLocation == null)
+        {
+            latestLocation = e.Location;
+            return;
+        }
+
+        if (!isMouseDown)
+        {
+            latestLocation = e.Location;
+            return;
+        }
+
+        if (GILauncher.TryGetProcess(out Process? p))
+        {
+            nint hWnd = p.MainWindowHandle;
+
+            if (User32.GetWindowRect(hWnd, out RECT windowRect)
+             && User32.GetClientRect(p.MainWindowHandle, out RECT clientRect))
+            {
+                if ((windowRect.bottom - windowRect.top) != (clientRect.bottom - clientRect.top))
+                {
+                    return;
+                }
+
+                Point lp = latestLocation ?? default;
+                Point cp = e.Location;
+
+                if (windowRect.ContainsUpper(lp, 35))
+                {
+                    _ = User32.MoveWindow(p.MainWindowHandle, windowRect.X + cp.X - lp.X, windowRect.Y + cp.Y - lp.Y, windowRect.Width, windowRect.Height, false);
+                }
+            }
+        }
+        latestLocation = e.Location;
+    }
+}
+
+file static class RectExtensions
+{
+    public static bool Contains(this RECT rect, POINT point)
+    {
+        return point.x >= rect.left && point.x <= rect.right && point.y >= rect.top && point.y <= rect.bottom;
+    }
+
+    public static bool ContainsUpper(this RECT rect, POINT point, int height)
+    {
+        return point.x >= rect.left && point.x <= rect.right && point.y >= rect.top && point.y <= (rect.top + height);
+    }
+}
