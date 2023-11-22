86b390093cb4fff81c119d0ef29700ec90a20d73
ema <mccoy39082@163.com>
2023-11-23 01:23:20 +0800

    feat: enhance borderless func
    1. Double Alt to focus next window.
    2. Game system menu to close/min/max window.

--- src/Desktop/Fischless.Fetch/DragMove/DragMoveProvider.cs
+++ src/Desktop/Fischless.Fetch/DragMove/DragMoveProvider.cs
@@ -1,5 +1,8 @@
 ﻿using Fischless.Fetch.Launch;
+using Fischless.KeyboardCapture;
+using Fischless.Logging;
 using Fischless.MouseCapture;
+using Fischless.Native;
 using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
@@ -30,6 +33,9 @@ public static class DragMoveProvider
                     MouseReader.Default.MouseUp += OnMouseUp;
                     MouseReader.Default.MouseMove += OnMouseMove;
                     MouseReader.Default.Start();
+
+                    KeyboardReader.Default.Received += OnKeyReceived;
+                    KeyboardReader.Default.Start();
                 }
                 else
                 {
@@ -37,6 +43,9 @@ public static class DragMoveProvider
                     MouseReader.Default.MouseUp -= OnMouseUp;
                     MouseReader.Default.MouseMove -= OnMouseMove;
                     MouseReader.Default.Stop();
+
+                    KeyboardReader.Default.Received -= OnKeyReceived;
+                    KeyboardReader.Default.Stop();
                 }
             }
         }
@@ -44,15 +53,71 @@ public static class DragMoveProvider
 
     private static Point? latestLocation = null;
     private static bool isMouseDown = false;
+    private static bool isHover = false;
 
     private static void OnMouseDown(object? sender, MouseEventArgs e)
     {
+        if (e.Button == MouseButtons.Left)
+        {
+            HMENU hMenu = User32.GetSystemMenu(Process.GetCurrentProcess().MainWindowHandle, false);
+
+            if (!hMenu.IsNull)
+            {
+                //_ = User32.DestroyMenu(hMenu);
+            }
+        }
         isMouseDown = true;
     }
 
     private static void OnMouseUp(object? sender, MouseEventArgs e)
     {
         isMouseDown = false;
+
+        if (isHover && e.Button == MouseButtons.Right)
+        {
+            if (GILauncher.TryGetProcess(out Process? p))
+            {
+                nint hWnd = Process.GetCurrentProcess().MainWindowHandle;
+                HMENU hMenu = User32.GetSystemMenu(hWnd, false);
+
+                if (hMenu.IsNull)
+                {
+                    hMenu = User32.CreatePopupMenu();
+                    _ = User32.AppendMenu(hMenu, User32.MenuFlags.MF_STRING, (nint)User32.SysCommand.SC_CLOSE, "Close");
+                    _ = User32.AppendMenu(hMenu, User32.MenuFlags.MF_STRING, (nint)User32.SysCommand.SC_MINIMIZE, "Minimize");
+                    _ = User32.AppendMenu(hMenu, User32.MenuFlags.MF_STRING, (nint)User32.SysCommand.SC_MAXIMIZE, "Maximize");
+                    _ = User32.SetMenu(hWnd, hMenu);
+                }
+
+                if (User32.GetCursorPos(out POINT pt))
+                {
+                    _ = User32.EnableMenuItem(hMenu, (uint)User32.SysCommand.SC_MINIMIZE, User32.MenuFlags.MF_ENABLED);
+                    _ = User32.EnableMenuItem(hMenu, (uint)User32.SysCommand.SC_MAXIMIZE, User32.MenuFlags.MF_ENABLED);
+                    uint command = User32.TrackPopupMenuEx(hMenu, User32.TrackPopupMenuFlags.TPM_RETURNCMD, (int)DpiHelper.CalcDPiX(pt.X), (int)DpiHelper.CalcDPiY(pt.Y), Process.GetCurrentProcess().MainWindowHandle, default);
+
+                    if (command == (uint)User32.SysCommand.SC_CLOSE)
+                    {
+                        try
+                        {
+                            p.Kill();
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex);
+                        }
+                    }
+                    else if (command == (uint)User32.SysCommand.SC_MINIMIZE)
+                    {
+                        _ = User32.ShowWindow(p.MainWindowHandle, ShowWindowCommand.SW_MINIMIZE);
+                    }
+                    else if (command == (uint)User32.SysCommand.SC_MAXIMIZE)
+                    {
+                        Screen screen = Screen.FromHandle(p.MainWindowHandle);
+                        _ = User32.SetWindowPos(p.MainWindowHandle, IntPtr.Zero, screen.Bounds.X, screen.Bounds.Y, screen.Bounds.Width, screen.Bounds.Height, User32.SetWindowPosFlags.SWP_NOZORDER);
+                    }
+                }
+            }
+        }
     }
 
     private static void OnMouseMove(object? sender, MouseEventArgs e)
@@ -74,6 +139,11 @@ public static class DragMoveProvider
             return;
         }
 
+        if (e.Button == MouseButtons.Right)
+        {
+            return;
+        }
+
         if (GILauncher.TryGetProcess(out Process? p))
         {
             nint hWnd = p.MainWindowHandle;
@@ -91,11 +161,40 @@ public static class DragMoveProvider
 
                 if (windowRect.ContainsUpper(lp, 35))
                 {
+                    isHover = true;
                     _ = User32.MoveWindow(p.MainWindowHandle, windowRect.X + cp.X - lp.X, windowRect.Y + cp.Y - lp.Y, windowRect.Width, windowRect.Height, false);
+                }
+                else
+                {
+                    isHover = false;
                 }
             }
         }
         latestLocation = e.Location;
+    }
+
+    private static DateTime latestAlt = DateTime.MinValue;
+
+    private static void OnKeyReceived(object? sender, KeyboardResult e)
+    {
+        if (e.IsAlt)
+        {
+            int interval = (int)(e.KeyboardItem.DateTime - latestAlt).TotalMilliseconds;
+
+            if (interval > 100 && interval <= 500)
+            {
+                HWND hWnd = User32.GetForegroundWindow();
+                _ = User32.GetWindowThreadProcessId(hWnd, out uint pid);
+                Process process = Process.GetProcessById((int)pid);
+
+                if (process != null && (process.ProcessName == GILauncher.ProcessNameCN || process.ProcessName == GILauncher.ProcessNameOVERSEA))
+                {
+                    User32.SwitchToThisWindow(User32.GetWindow(hWnd, User32.GetWindowCmd.GW_HWNDNEXT), true);
+                }
+            }
+
+            latestAlt = e.KeyboardItem.DateTime;
+        }
     }
 }
 
