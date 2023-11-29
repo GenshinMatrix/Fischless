cf2875d2604e2586a9e7ef8e85f4ae283ec25bbd
ema <mccoy39082@163.com>
2023-11-29 11:30:36 +0800

    sync: MicaSetup commit `254ae93`

--- build/MicaSetup/Resources/Fonts/IcoMoon/create.cmd
+++
@@ -1,9 +0,0 @@
-cd /d %~dp0
-@REM npm i -g icomoon-cli
-@REM icomoon-cli -h
-@REM icomoon-cli -i 1.svg -s selection.json -n 1 -o output
-@REM icomoon-cli -i "%~1" -s selection.json -n "%~n1" -o output
-for %%i in (%*) do (
-  icomoon-cli -i "%%~i" -s selection.json -n "%%~ni" -o output
-)
-@pause
--- build/MicaSetup/Resources/Fonts/IcoMoon/overlay.cmd
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-copy /y .gitignore .\output\
-copy /y .gitattributes .\output\
-copy /y .gitattributes .\output\demo-files\
-copy /y .\output\selection.json .
-@pause
