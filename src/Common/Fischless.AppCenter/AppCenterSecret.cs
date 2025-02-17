f8e60ef9c83a275e18de8b7b5de03f3fcba8a826
ema <mccoy39082@163.com>
2025-02-17 22:17:54 +0800

    chore: remove AppCenter

--- src/Common/Fischless.AppCenter/AppCenterSecret.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fischless;
-
-public class AppCenterSecret
-{
-    public string Owner { get; set; }
-    public string AppName { get; set; }
-    public string UserId { get; set; }
-    public string AppSecret { get; set; }
-}
